2ace75374321c8ab02ed6348363d2612515d9978
Sato Mafuyu <0000110530372xsp@al.educacao.sp.gov.br>
2024-11-29 13:27:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital da Japão é Tóquio'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Japonesa',
+    'Como se diz bom dia em Japonês?',
+    'Bom dia em japonês é OHAYO (おはよう)'
+)
